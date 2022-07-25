81384e3100b43f28e557c6e1791b3aa3ddf3bd1e
Arabeseque <hzy1210085279@gmail.com>
2022-07-25 09:49:12 +0800

    feat: upload

--- components.d.ts
+++ components.d.ts
@@ -7,16 +7,10 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    CompDom: typeof import('./src/components/mainZone/components/compDom.vue')['default']
     Component: typeof import('./src/components/component.vue')['default']
-    ComponentData: typeof import('./src/components/LeftZone/components/componentData.vue')['default']
     Counter: typeof import('./src/components/Counter.vue')['default']
-    Editor: typeof import('./src/components/Editor.vue')['default']
-    ElButton: typeof import('element-plus/es')['ElButton']
+    Editor: typeof import('./src/components/Editor.jsx')['default']
     Footer: typeof import('./src/components/Footer.vue')['default']
-    LeftZone: typeof import('./src/components/LeftZone/index.vue')['default']
-    Main: typeof import('./src/components/main/index.vue')['default']
-    MainZone: typeof import('./src/components/mainZone/index.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
