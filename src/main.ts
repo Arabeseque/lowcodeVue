32592287e2a498fb556e31fbc18d2e24eedbfc3c
RioHuang <hzy1210085279@gmail.com>
2022-07-27 09:55:41 +0800

    feat: 减少不必要依赖， 增加部分注释

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from 'virtual:generated-pages'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
+
+// 暂时使用该组件库, mainZone/comonents/compoDom.vue
 import ydH5 from 'ydh5-ui'
 
 const app = createApp(App)
@@ -14,9 +15,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
-for (const [key, component] of Object.entries(ElementPlusIconsVue))
-  app.component(key, component)
-
 const pinia = createPinia()
 
 app.use(router)
